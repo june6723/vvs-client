7318d23c68927a6099587bfd92d85822d06d71af
june6723 <june6723@naver.com>
2021-04-23 00:37:09 +0900

    changes server url

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const LOCAL = "http://localhost:5000";
 const HEROKU = "https://vvs-backend.herokuapp.com";
-const API = axios.create({ baseURL: LOCAL });
+const API = axios.create({ baseURL: HEROKU });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('token')) {
